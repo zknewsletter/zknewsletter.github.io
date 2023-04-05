ebee1018c77b0ecbe44c01d5f0eed725de4a8cea
zie1ony <maciej@odra.dev>
2023-04-05 17:03:32 +0200

    2023-04-05

--- zkn-builder/src/main.rs
+++ zkn-builder/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs, path::PathBuf};
 
 fn main() {
-    let issue = fs::read_to_string(PathBuf::from("../issues/2023-03-29.md")).unwrap();
+    let issue = fs::read_to_string(PathBuf::from("../issues/2023-04-05.md")).unwrap();
     println!("{}", markdown::to_html(&issue));
 }
